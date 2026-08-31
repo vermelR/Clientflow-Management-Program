8efbbc846925b6074bd2790a3e18d6422977de3c
vermelR <96270860+vermelR@users.noreply.github.com>
2026-08-31 14:38:49 -0400

    Update firebase-config.js
    Added firebase

--- firebase-config.js
+++ firebase-config.js
@@ -30,6 +30,30 @@ window.DJCF_FIREBASE_CONFIG = {
   appId: "YOUR_APP_ID",
 };
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBj92ExYd28fk4dhFZW_60N1aHzXnmLg0I",
+    authDomain: "rnd---client-management-b21e5.firebaseapp.com",
+    projectId: "rnd---client-management-b21e5",
+    storageBucket: "rnd---client-management-b21e5.firebasestorage.app",
+    messagingSenderId: "285914763098",
+    appId: "1:285914763098:web:bd31ca07dbe0169363ad63",
+    measurementId: "G-TJ0K83HDB2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
 /* Optional branding for the login screen of your hosted app. */
 window.DJCF_APP_INFO = {
   productName: "DJ ClientFlow",
